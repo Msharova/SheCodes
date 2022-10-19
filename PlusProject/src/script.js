1c784fd5b129f396da5269a8528da0d5de58b61d
Msharova <sharova.maria.3d@gmail.com>
2022-10-19 16:02:31 +0200

    updated all files, mainly js

--- PlusProject/src/script.js
+++ PlusProject/src/script.js
@@ -1,1 +1,49 @@
-//here will be our majestic code
+//changing current day and time
+let now = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tueday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let mainDay = days[now.getDay()];
+let hour = now.getHours();
+let minutes = now.getMinutes();
+
+let currentDay = document.querySelector("#mainDay");
+currentDay.innerHTML = `${mainDay}, `;
+
+let currentTime = document.querySelector("#mainTime");
+currentTime.innerHTML = `${hour}:${minutes}`;
+
+//changing submited city
+
+function citySearch(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector(".searchEngine");
+  let currentCity = document.querySelector(".cityName");
+  currentCity.innerHTML = cityInput.value;
+}
+
+let searchEngine = document.querySelector(".location-elements");
+searchEngine.addEventListener("submit", citySearch);
